8a95f67f3370db1d626f5591d84b625ad704f1d1
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-08-08 16:37:35 +0500

    building v1

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 mcp = FastMCP(name="Phloem")
+
+if __name__ == "__main__":
+    mcp.run(
+        transport="http",
+        host="0.0.0.0",
+        port=8000,
+        middleware=[
+            Middleware(
+                CORSMiddleware,
+                allow_origins=["*"],
+                allow_credentials=True,
+                allow_methods=["*"],
+                allow_headers=["*"],
+            )
+        ]
+    )
